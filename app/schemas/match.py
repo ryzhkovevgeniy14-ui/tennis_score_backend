19da5348bf823f03b68a3b7ef81e5ad6c60cbbe2
Red-14 <red-14@gmail.com>
2026-05-05 01:55:45 +0300

    add match schema

---
+++ app/schemas/match.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class MatchCreate(BaseModel):
+    player_1_name: str
+    player_2_name: str
+
+
+class Match(BaseModel):
+    match_id: int
+    players: list[str]
+    current_set: dict[str, int]
+    sets: list[int]
+    history_sets: list[dict[str, int]]
+    server: str
+
+
+class GameUpdate(BaseModel):
+    player: str
+    count: int
