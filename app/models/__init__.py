37166416a9f521cba4db4cdd90a000af2d0c7fd8
Red-14 <red-14@gmail.com>
2026-05-05 14:44:03 +0300

    Register all ORM models in models module

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from .player import Player
+from .match import Match
+from .player_stats import PlayerStats
+from .sets_history import SetHistory
+
+
+__all__ = ["Player", "Match", "PlayerStats", "SetHistory"]
