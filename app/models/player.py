0c05e2a9674633f63904e14a6d9318d7a4a919a9
Red-14 <red-14@gmail.com>
2026-05-06 16:46:43 +0300

    add relationships between Player, Match and PlayerStats

--- app/models/player.py
+++ app/models/player.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String, DateTime, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from datetime import datetime
 
@@ -12,3 +12,8 @@ class Player(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True, index=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+    # Связи
+    matches_as_player1 = relationship("Match", foreign_keys="Match.player1_id", back_populates="player1")
+    matches_as_player2 = relationship("Match", foreign_keys="Match.player2_id", back_populates="player2")
+    stats = relationship("PlayerStats", back_populates="player", uselist=False)
