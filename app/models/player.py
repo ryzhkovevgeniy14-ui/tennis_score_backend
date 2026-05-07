ecb4703c00c9f0c3a28f0dfc10bf216741cf8fb0
Red-14 <red-14@gmail.com>
2026-05-07 20:27:56 +0300

    fix ORM model Player

--- app/models/player.py
+++ app/models/player.py
@@ -1,7 +1,5 @@
-from sqlalchemy import String, DateTime, func
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-from datetime import datetime
 
 from app.db.base import Base
 
@@ -9,11 +7,18 @@ from app.db.base import Base
 class Player(Base):
     __tablename__ = "players"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)
-    name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True, index=True)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
 
-    # Связи
-    matches_as_player1 = relationship("Match", foreign_keys="Match.player1_id", back_populates="player1")
-    matches_as_player2 = relationship("Match", foreign_keys="Match.player2_id", back_populates="player2")
-    stats = relationship("PlayerStats", back_populates="player", uselist=False)
+
+    # Связь один ко многим (1 игрок - много матчей)
+    matches_player1: Mapped[list["Match"]] = relationship(
+        "Match",
+        foreign_keys="Match.player1_id",
+        back_populates="player1"
+    )
+    matches_player2: Mapped[list["Match"]] = relationship(
+        "Match",
+        foreign_keys="Match.player2_id",
+        back_populates="player2"
+    )
