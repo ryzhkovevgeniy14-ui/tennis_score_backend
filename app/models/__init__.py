7c562e1cdd20851e872ee262ef0da46fc2080877
Red-14 <red-14@gmail.com>
2026-05-08 19:42:16 +0300

    add new ORM model Player Stats

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
 from .player import Player
 from .match import Match
 from .set_history import SetHistory
+from .player_stats import PlayerStats
 
 
-__all__ = ["Player", "Match", "SetHistory"]
+__all__ = ["Player", "Match", "SetHistory", "PlayerStats"]
