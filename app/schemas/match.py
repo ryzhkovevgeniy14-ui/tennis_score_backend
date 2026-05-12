3ad9a64f18f7a1cc6120fe088b13fe4cdcc4bab5
Red-14 <red-14@gmail.com>
2026-05-12 17:48:05 +0300

    add Annotated with Field descriptions to schemas

--- app/schemas/match.py
+++ app/schemas/match.py
@@ -1,26 +1,39 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Annotated
 
 
 class MatchCreate(BaseModel):
-    player1_name: str
-    player2_name: str
+    """
+    Модель для создания матча.
+    Используется в POST-запросах.
+    """
+    player1_name: Annotated[str, Field(description="Имя игрока 1")]
+    player2_name: Annotated[str, Field(description="Имя игрока 2")]
 
 
 class MatchResponse(BaseModel):
-    id: int
-    player1_name: str
-    player2_name: str
-    games_p1: int
-    games_p2: int
-    sets_p1: int
-    sets_p2: int
-    tiebreak: bool
-    server_name: str
-    history_sets: list[dict[str, int]] = []
+    """
+    Модель для ответа с данными матча.
+    Используется в GET-запросах.
+    """
+    id: Annotated[int, Field(description="ID матча")]
+    player1_name: Annotated[str, Field(description="Имя игрока 1")]
+    player2_name: Annotated[str, Field(description="Имя игрока 2")]
+    games_p1: Annotated[int, Field(description="Количество геймов игрока 1")]
+    games_p2: Annotated[int, Field(description="Количество геймов игрока 2")]
+    sets_p1: Annotated[int, Field(description="Количество сетов игрока 1")]
+    sets_p2: Annotated[int, Field(description="Количество сетов игрока 2")]
+    tiebreak: Annotated[bool, Field(description="Наличие тайбрейка")]
+    server_name: Annotated[str, Field(description="Подающий")]
+    history_sets: Annotated[list[dict[str, int]], Field(default=[], description="История сыгранных сетов")]
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class GameUpdate(BaseModel):
-    player_name: str
-    count: int
+    """
+    Модель для изменения счёта.
+    Используется в POST-запросах.
+    """
+    player_name: Annotated[str, Field(description="Имя игрока")]
+    count: Annotated[int, Field(description="Количество геймов")]
--- app/schemas/player.py
+++ app/schemas/player.py
@@ -1,12 +1,29 @@
 from pydantic import BaseModel, Field, ConfigDict
+from typing import Annotated
 
 
 class PlayerCreate(BaseModel):
-    name: str = Field(..., min_length=1, max_length=50, pattern=r'^[a-zA-Zа-яА-ЯёЁ\s\-]+$')
+    """
+    Модель для добавления игрока.
+    Используется в POST-запросах.
+    """
+    name: Annotated[
+        str,
+        Field(
+            min_length=1,
+            max_length=50,
+            pattern=r'^[a-zA-Zа-яА-ЯёЁ\s\-]+$',
+            description="Имя игрока"
+        )
+    ]
 
 
 class Player(BaseModel):
-    id: int
-    name: str
+    """
+    Модель для ответа с данными игрока.
+    Используется в GET-запросах.
+    """
+    id: Annotated[int, Field(description="ID игрока")]
+    name: Annotated[str, Field(description="Имя игрока")]
 
     model_config = ConfigDict(from_attributes=True)
