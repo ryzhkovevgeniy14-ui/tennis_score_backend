cf2335f5b2971d545fede68b9fe0592bfa328234
Red-14 <red-14@gmail.com>
2026-05-12 17:44:36 +0300

    refactor commentary

--- app/core/config.py
+++ app/core/config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 # Загружаем переменные окружения из файла .env
 load_dotenv()
 
-# URL подключения к БД. В Docker используется db:5432, локально localhost:5432
+# URL подключения к БД
 DATABASE_URL: str = os.getenv("DATABASE_URL")
